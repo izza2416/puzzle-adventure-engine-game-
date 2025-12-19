5325e4e283c2d83ecb3cb5414e5d747c5d1b1d01
izza2416 <izzarana360@gmail.com>
2025-12-19 16:26:07 +0500

    Second commit

--- projectcs101.cpp
+++ projectcs101.cpp
@@ -1,3 +1,22 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
+
+const int roomcount = 5;
+const int invenmax = 10;
+
+string roomNames[roomcount] = {
+    "FCSE",
+    "ACB",
+    "CAFE",
+    "TUC",
+    "LIBRARY"
+};
+
+string roomDescriptions[roomcount] = {
+    "A building where offices,lecture hall and labs are situated. ",
+    "A building where many students are taking lectures.",
+    "A place where tea and snacks are getting sale .",
+    "place where many foods are being served and a reg desk is setted up.",
+    "students are studing there ."
+};
