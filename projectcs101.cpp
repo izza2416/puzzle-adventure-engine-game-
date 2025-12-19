13be4ad1d144b17dde064f655c3ee7cbe1b2ed57
izza2416 <izzarana360@gmail.com>
2025-12-19 16:40:21 +0500

    third commit

--- projectcs101.cpp
+++ projectcs101.cpp
@@ -20,3 +20,19 @@ string roomDescriptions[roomcount] = {
     "place where many foods are being served and a reg desk is setted up.",
     "students are studing there ."
 };
+string puzzleQuestions[roomcount] = {
+    "what is 2+2 ",
+    "What comes after d",
+    "A shape with no corners",
+    "I m white but i have yellow inside me . i am oval guess who i am ?",
+    "LIFE is not a bed of ------?"
+};
+
+
+string puzzleAnswers[roomcount] = {
+    "4",
+    "e",
+    "circle",
+    "egg",
+    "roses"
+};
